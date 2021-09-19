c6a1dbc0f1ee683816337544ce7a4aae9f7108f3
Artur Donda <artur.donda@outlook.com>
2021-09-19 10:05:12 -0300

    Removing IP logs 2

--- src/utils/ipData.ts
+++ src/utils/ipData.ts
@@ -3,23 +3,16 @@ import { IIpData } from '../models';
 
 const ipdata = new IPData(process.env.IPDATA_KEY);
 
-export const getIpInfo = (ipAddress: string): Promise<IIpData> => {
-	console.log('Getting info about IP: ', ipAddress);
-
-	return ipdata.lookup(ipAddress).then(ipInfo => {
-		console.log('Results: ', ipInfo);
-
-		return {
-			ip: ipInfo.ip ?? '',
-			countryName: ipInfo.country_name ?? '',
-			countryCode: ipInfo.country_code ?? '',
-			countryFlag: ipInfo.emoji_flag ?? '',
-			regionName: ipInfo.region ?? '',
-			regionCode: ipInfo.region_code ?? '',
-			city: ipInfo.city ?? '',
-		};
-	});
-};
+export const getIpInfo = (ipAddress: string): Promise<IIpData> =>
+	ipdata.lookup(ipAddress).then(ipInfo => ({
+		ip: ipInfo.ip ?? '',
+		countryName: ipInfo.country_name ?? '',
+		countryCode: ipInfo.country_code ?? '',
+		countryFlag: ipInfo.emoji_flag ?? '',
+		regionName: ipInfo.region ?? '',
+		regionCode: ipInfo.region_code ?? '',
+		city: ipInfo.city ?? '',
+	}));
 
 export const getIpInfoBulk = (ipAddressList: string[]): Promise<IIpData[]> =>
 	ipdata.bulkLookup(ipAddressList).then(ipInfoList =>
